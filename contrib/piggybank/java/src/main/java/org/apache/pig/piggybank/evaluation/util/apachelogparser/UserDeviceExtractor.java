575b8e8c748f8e7a188796bd0e3eb0420b61b855
d601 <shmeep@gmail.com>
2015-10-28 11:10:33 -0700

    Copy over user agent/device parser code from old piggybank, add uadetector as dep

---
+++ contrib/piggybank/java/src/main/java/org/apache/pig/piggybank/evaluation/util/apachelogparser/UserDeviceExtractor.java
@@ -0,0 +1,40 @@
+package org.apache.pig.piggybank.evaluation.util.apachelogparser;
+
+import net.sf.uadetector.ReadableUserAgent;
+import net.sf.uadetector.UserAgentStringParser;
+import net.sf.uadetector.service.UADetectorServiceFactory;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+public class UserDeviceExtractor extends EvalFunc<String> {
+	
+	public String exec(Tuple tuple) { //throws IOException {
+	    if (tuple == null || tuple.size() < 1) {
+	      return null;
+	    }
+	    try {
+	      String refAgent = (String) tuple.get(0);
+	      return deviceString(refAgent);
+	    } catch (Exception e) {
+	      //catch (ExecException ee) {
+	      //throw new IOException(ee);
+	      return "ERROR PARSING USER DEVICE";
+	    }
+	  }
+	
+	  /*
+	   * This method has been added to piggybank to evaluate the user agent to parse the device type.
+	  */
+	  private String deviceString(String refAgent) {
+		  try
+		  {
+			  UserAgentStringParser parser = UADetectorServiceFactory.getCachingAndUpdatingParser();
+			  ReadableUserAgent rAgent = parser.parse(refAgent);
+			  return rAgent.getDeviceCategory().getName();
+		  } catch (Exception ignore) {
+			  return null;
+		  }
+	  }
+
+}
